eea1c62b29f6bcb1785fa22ef9a4de6435084f12
dylandotolo <dylan.dotolo@okstate.edu>
2017-04-13 13:59:27 -0500

    Started building out the first activity.

--- app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
+++ app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
@@ -2,6 +2,8 @@ package com.winetenders.msis4363.winetenders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-    //testing git push delete whenever =)
+        // Testing the dropdown spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.wines_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
-    //test on git =)
+    }
+
 }
