76166aea6968a57c12b360d72264c3de3da1bb27
dylandotolo <dylan.dotolo@okstate.edu>
2017-04-13 14:58:06 -0500

    Laid out all necessary components on the activity_main.xml

--- app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
+++ app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Testing the dropdown spinner
+        // Testing the dropdown spinner with fake data
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -21,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        // Testing the dropdown spinner with fake data
+        Spinner priceSpinner = (Spinner) findViewById(R.id.priceSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> priceAdapter = ArrayAdapter.createFromResource(this,
+                R.array.price_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        priceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        priceSpinner.setAdapter(priceAdapter);
 
     }
 
