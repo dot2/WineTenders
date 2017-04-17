28a4a9922c599aa088ea1960d7df319f3093f06a
dylandotolo <dylan.dotolo@okstate.edu>
2017-04-17 15:30:12 -0500

    Aligned all elements and added fake data to populate the spinners.

--- app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
+++ app/src/main/java/com/winetenders/msis4363/winetenders/MainActivity.java
@@ -32,6 +32,36 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         priceSpinner.setAdapter(priceAdapter);
 
+        // Testing the dropdown spinner with fake data
+        Spinner sweetnessSpinner = (Spinner) findViewById(R.id.sweetnesSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> sweetnessAdapter = ArrayAdapter.createFromResource(this,
+                R.array.sweetness_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        sweetnessAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sweetnessSpinner.setAdapter(sweetnessAdapter);
+
+        // Testing the dropdown spinner with fake data
+        Spinner acidicSpinner = (Spinner) findViewById(R.id.aciditySpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> acidicAdapter = ArrayAdapter.createFromResource(this,
+                R.array.acidity_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        acidicAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        acidicSpinner.setAdapter(acidicAdapter);
+
+        // Testing the dropdown spinner with fake data
+        Spinner bitternessSpinner = (Spinner) findViewById(R.id.bitternessSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> bitternessAdapter = ArrayAdapter.createFromResource(this,
+                R.array.bitterness_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        bitternessAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        bitternessSpinner.setAdapter(bitternessAdapter);
+
     }
 
 }
